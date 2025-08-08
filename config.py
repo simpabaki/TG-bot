6d523b422044e317746099b4bc835a7996b3a8d8
simpabaki <simpabaki@gmail.com>
2025-08-08 13:04:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-SHEET_NAME = 'https://docs.google.com/spreadsheets/d/1azIdDEROVrx_L8ncQsTtyqsvZWorcPmWhjOouUS-Pxo/edit?gid=1540510432#gid=1540510432'  # Замените на вашу таблицу
+SHEET_NAME = 'бот для отзывов'  # Замените на вашу таблицу
