5f18b3fed9950603b8f1b76bf680264beac10b66
simpabaki <simpabaki@gmail.com>
2025-08-08 16:29:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 import uvicorn
 import os
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import Update
 
@@ -12,13 +12,11 @@ from handlers import router
 
 app = FastAPI()
 
-# Получаем конфиг из таблицы
 config = get_config(SHEET_NAME)
 BOT_TOKEN = config["bot_token"]
 WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
-WEBHOOK_URL = f"{os.environ.get('WEBHOOK_BASE')}{WEBHOOK_PATH}"
+WEBHOOK_URL = f"{os.getenv('WEBHOOK_BASE')}{WEBHOOK_PATH}"
 
-# Настраиваем бота и диспетчер
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
 dp.include_router(router)
@@ -40,8 +38,4 @@ async def telegram_webhook(request: Request):
     data = await request.json()
     update = Update.model_validate(data)
     await dp.feed_update(bot, update)
-    return {"ok": True}
-
-def start_api_server():
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
+    return {"ok": True"}
