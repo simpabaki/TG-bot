baa55942ae006ad8c2b0d60f726613913462af8f
simpabaki <simpabaki@gmail.com>
2025-08-08 16:10:20 +0300

    Update gsheet.py

--- gsheet.py
+++ gsheet.py
@@ -1,25 +1,25 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import datetime
 import os
 import json
-
-def get_gsheet_client():
-    scope = [
-        'https://spreadsheets.google.com/feeds',
-        'https://www.googleapis.com/auth/drive'
-    ]
-    creds_dict = json.loads(os.environ['GOOGLE_APPLICATION_CREDENTIALS_JSON'])
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    return gspread.authorize(creds)
+import gspread
+from google.oauth2.service_account import Credentials
 
 def get_config(sheet_name):
-    gc = get_gsheet_client()
-    sheet = gc.open(sheet_name).worksheet('config')
-    config_data = sheet.get_all_records()
-    return {row['key']: row['value'] for row in config_data}
+    credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON_STRING")
+    if not credentials_json:
+        raise ValueError("Переменная GOOGLE_CREDENTIALS_JSON_STRING не установлена.")
 
-def save_user_data(sheet_name, user_id, username, full_name, phone, status):
-    gc = get_gsheet_client()
-    sheet = gc.open(sheet_name).worksheet('users')
-    sheet.append_row([user_id, username, full_name, phone, status, datetime.datetime.now().isoformat()])
+    credentials_dict = json.loads(credentials_json)
+    creds = Credentials.from_service_account_info(credentials_dict)
+    gc = gspread.authorize(creds)
+
+    sheet = gc.open(sheet_name).worksheet("config")
+
+    config = {
+        "bot_token": sheet.acell("A2").value,
+        "admin_id": sheet.acell("B2").value,
+        "approve_text": sheet.acell("C2").value,
+        "reject_text": sheet.acell("D2").value,
+        "mini_course_link": sheet.acell("E2").value,
+    }
+    return config
+
